3c3ff0033dd146b024cd1afbcdef1435debbd353
Kurenkov Maxim <159890193+KurenkovMaxgit@users.noreply.github.com>
2025-08-28 16:00:39 +0300

    Prettier added

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 
 app.use(express.json());
-// app.use("/api", authorRoutes);
-// app.use("/api", bookRoutes);
-// app.use("/api", orderRoutes);
-// app.use("/api", studentRoutes);
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
