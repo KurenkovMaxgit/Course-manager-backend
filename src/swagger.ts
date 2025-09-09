6e3fe9cc9ce555fd2060862b57452b36ca522e2b
Kurenkov Maxim <159890193+KurenkovMaxgit@users.noreply.github.com>
2025-09-09 05:26:46 +0300

    Minor host port fix

--- src/swagger.ts
+++ src/swagger.ts
@@ -10,7 +10,7 @@ const options: swaggerJSDoc.Options = {
     },
     servers: [
       {
-        url: "http://localhost:3000/api",
+        url: `http://localhost:${process.env.PORT}/api`,
       },
     ],
     components: {
